0be536b6147e3da05592abab01aca6600fdf8b25
FRA <>
2023-10-05 11:05:03 -0400

    feat: working on connecting dll apis using ctypes

---
+++ HID_RELAY.md
@@ -0,0 +1,8 @@
+# Introduction to Human Interface Device
+
+More HID info can be found at [Learn Microsoft](https://www.bing.com/search?pglt=41&q=how+to+add+link+in+markdown&cvid=fe534dd52f9d43d1a2e92fb7b5f8ac81&gs_lcrp=EgZjaHJvbWUqBAgAEAAyBAgAEAAyBggBEEUYOTIECAIQADIECAMQADIECAQQADIECAUQADIECAYQADIECAcQADIECAgQANIBCDM5MzVqMGoxqAIAsAIA&FORM=ANNTA1&PC=U531)
+
+- USB RELAY
+    - vendor: `0x16c0`
+    - device id: `0x05DF`
+---
---
+++ hid/hid_dll_api.py
@@ -0,0 +1,31 @@
+import ctypes
+from ctypes import Structure, Union, c_ubyte, c_long, c_ulong, c_ushort, \
+        c_wchar, c_void_p, c_uint 
+from ctypes.wintypes import ULONG, BOOLEAN, BYTE, WORD, DWORD, HANDLE, BOOL, \
+        WCHAR, LPWSTR, LPCWSTR
+hid_dll = ctypes.windll.hid
+
+class GUID_BUFFER(ctypes.Structure):
+    """ 
+        Globally Unique Identifier Buffer
+        --- 
+
+        Pointer to a caller-allocated GUID buffer 
+        that the routine uses to return the device
+        interface GUID for HIDClass devices.
+        retrieved from https://learn.microsoft.com/ 
+
+    Args:
+        ctypes (_type_): Structure data type frm C
+    """
+    _pack_ = 1
+    _fields_ = [
+        ("data1", DWORD), # Unsigned 4 bytes or 32 bits of data
+        ("data2", WORD), # Unsigned 2 bytes or 16 bits of data 
+        ("data3", WORD), 
+        ("data4", BYTE * 8),
+    ]
+
+
+
+print(hid_dll.HidD_GetHidGuid(ctypes.byref(GUID_BUFFER())))
